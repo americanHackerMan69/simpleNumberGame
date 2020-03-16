135a20ea94f04200ce420f754e0834ee05905456
americanHackerMan69 <62257828+americanHackerMan69@users.noreply.github.com>
2020-03-16 13:15:42 -0400

    version 1.0
    lol

---
+++ test.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+print("Welcome to Nicholas's Random Number game!")
+print("")
+raw_input("Press any key to continue....")
+
+number = random.randint(1,100)
+chances = 5
+
+a = input("From 1 to 100, what is the number?: ")
+
+if a>100:
+    while True:
+       print("YOU CANT GUESS ABOVE 100!!!")
+       a = input("From 1 to 100, what is the number?: ")
+       if a<101:
+           break
+
+if a < 1:
+    while True:
+        print("YOU CANT GUESS BELOW 1!!!!")
+        a = input("From 1 to 100, what is the number?: ")
+        if a>0:
+            break
+
+if a==number:
+    print("CORRECT. You win!")
+if a!=number:
+    chances = chances - 1
+    while True:
+        print("WRONG! You have " + str(chances) + " chances left")
+        chances = chances -1
+        a = input("From 1 to 100, what is the number?: ")
+        if a == number:
+            print("CORRECT. You win!")
+            break
+        if chances == 0 and a!=number:
+            print("WRONG! You lose! The real nunber was " + str(number))
+            break
+
+
+
+
